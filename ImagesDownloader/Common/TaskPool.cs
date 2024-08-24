52867fe991524ed6e818ccf56e40697aa63e1629
VNCompany <i@vneznanov.ru>
2024-08-24 23:15:25 +0300

    TasksPool

--- ImagesDownloader/Common/TaskPool.cs
+++ ImagesDownloader/Common/TaskPool.cs
@@ -1,6 +1,25 @@
 ﻿namespace ImagesDownloader.Common;
 
-public sealed class TaskPool
+public static class TaskPool
 {
-    private TaskPool() { }
+    public static async Task Run(
+        IEnumerable<Func<Task>> tasks, 
+        int poolSize, 
+        CancellationToken cancellationToken)
+    {
+        using var semaphore = new SemaphoreSlim(poolSize);
+        var taskList = new List<Task>();
+        foreach (Func<Task> func in tasks)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            taskList.Add(Task.Run(async () =>
+            {
+                await semaphore.WaitAsync(cancellationToken);
+                await func.Invoke();
+                semaphore.Release();
+            }, cancellationToken));
+        }
+
+        await Task.WhenAll(taskList);
+    }
 }
